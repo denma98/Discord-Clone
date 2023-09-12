c0a4e85bef58807b2b9564f5c40ca249b023ee69
denma <108463489+denma98@users.noreply.github.com>
2023-09-12 19:43:32 +0530

    Added dark mode

--- discord/app/(main)/(router)/page.tsx
+++ discord/app/(main)/(router)/page.tsx
@@ -1,8 +1,14 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      This is a protected route.(only for logged in users)
+      <UserButton
+        afterSignOutUrl="/"
+      /> 
+      <ModeToggle/>
+            
     </div>
   )
 }
