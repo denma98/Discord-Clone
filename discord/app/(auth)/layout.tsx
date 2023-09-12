0d6b9e21d3ff8af71c41d39f9be1009318a96678
denma <108463489+denma98@users.noreply.github.com>
2023-09-12 17:45:41 +0530

    Arranged the folders

--- discord/app/(auth)/layout.tsx
+++ discord/app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
 const AuthLayout = ({children}: {children: React.ReactNode}) => {
     return (  
-        <div className="bg-blue-550 h-full">
+        <div className="bg-red-550 h-full">
             {children}
         </div>
     );
 }
- 
+
 export default AuthLayout;
---
+++ discord/app/(main)/(router)/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      This is a protected route.(only for logged in users)
+    </div>
+  )
+}
