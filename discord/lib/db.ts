41ec781f439cb08d21816f05e4aee365b6c4a398
denma <108463489+denma98@users.noreply.github.com>
2023-09-14 01:45:08 +0530

    added prisma, made db schema

---
+++ discord/lib/db.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+declare global {
+    var prisma: PrismaClient | undefined
+}
+
+export const db = globalThis.prisma || new PrismaClient()
+
+if( process.env.NODE_ENV !=="production") globalThis.prisma = db
+// here we made a global variable so that everytime we relaod page, so that new prisma client is not initialised
