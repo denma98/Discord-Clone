024eef0de2b0107325f623a04b156d2f67ee2dfc
denma <108463489+denma98@users.noreply.github.com>
2023-09-17 02:06:41 +0530

    Added ui components like form, dialogue, input

---
+++ discord/components/modals/initial-modal.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+
+} from "@/components/ui/dialog"
+
+export const InitialModal = () => {
+    
+    return(
+        <Dialog open>
+
+        </Dialog>
+    )
+}
