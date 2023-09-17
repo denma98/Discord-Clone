af973428f0144bcc165be5167fdb14dc2aeb5891
denma <108463489+denma98@users.noreply.github.com>
2023-09-17 12:42:11 +0530

    added dialogue, form , form schema into the main page. added zod to verify server name and image with a message

--- discord/components/modals/initial-modal.tsx
+++ discord/components/modals/initial-modal.tsx
@@ -1,4 +1,7 @@
 "use client"
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import {zodResolver} from "@hookform/resolvers/zod"
 
 import {
     Dialog,
@@ -10,10 +13,38 @@ import {
 
 } from "@/components/ui/dialog"
 
+const fromSchema = z.object({
+    name: z.string().min(1, {
+        message: "Server name is required."
+    }),
+    imageUrl: z.string().min(1, {
+       message: "Server image is required."
+    })
+})
+
+
 export const InitialModal = () => {
+    
+    const form = useForm({
+        resolver: zodResolver(fromSchema),
+        defaultValues: {
+            name: "",
+            imageUrl: "",
+
+        }
+    })
     
     return(
         <Dialog open>
+            <DialogHeader className=" pt-8 px-6">
+                <DialogTitle className=" text-2xl  text-center">
+                    Customize your server
+                </DialogTitle>
+                <DialogDescription className=" text-center text-zinc-500">
+                    Give your server a personality with a name and an image.
+                    You can always change it later.
+                </DialogDescription>
+            </DialogHeader>
 
         </Dialog>
     )
