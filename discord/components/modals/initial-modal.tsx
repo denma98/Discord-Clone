36f29c6025e2a817fbec48c4e0ae47e7c4a042f9
denma <108463489+denma98@users.noreply.github.com>
2023-09-23 01:11:32 +0530

    Made form schema, added form and buttons also a warning to write server name

--- discord/components/modals/initial-modal.tsx
+++ discord/components/modals/initial-modal.tsx
@@ -1,6 +1,6 @@
 "use client"
-import * as z from "zod"
 import { useForm } from "react-hook-form"
+import * as z from "zod"
 import {zodResolver} from "@hookform/resolvers/zod"
 
 import {
@@ -13,6 +13,18 @@ import {
 
 } from "@/components/ui/dialog"
 
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form"
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
 const fromSchema = z.object({
     name: z.string().min(1, {
         message: "Server name is required."
@@ -24,18 +36,28 @@ const fromSchema = z.object({
 
 
 export const InitialModal = () => {
+
+    
     
     const form = useForm({
         resolver: zodResolver(fromSchema),
         defaultValues: {
             name: "",
             imageUrl: "",
-
+            
         }
     })
-    
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async(values: z.infer<typeof fromSchema>) =>{
+        console.log(values)
+    }
+
     return(
         <Dialog open>
+            <DialogContent className=" bg-white text-black p-0 overflow-hidden ">
+
             <DialogHeader className=" pt-8 px-6">
                 <DialogTitle className=" text-2xl  text-center">
                     Customize your server
@@ -45,7 +67,47 @@ export const InitialModal = () => {
                     You can always change it later.
                 </DialogDescription>
             </DialogHeader>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-8">
+                <div className=" space-y-8 px-8">
 
+                    <div className=" flex items-center justify-center text-center">
+                        TODO: image
+                    </div>
+
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel className=" uppercase text-xs font-bold text-zinc-500 dark: text-secondary/70"> 
+                                {/* xs -> extra small, /70 is for opacity */}
+                                    Server name
+                                </FormLabel>
+
+                                <FormControl>
+                                    <Input
+                                        className=" bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible: ring-offset-0
+                                        "
+                                        placeholder="Enter server name"
+                                        {...field}
+                                        />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                        /> 
+
+                </div>
+            <DialogFooter className=" bg-gray-100 px-6 py-4">
+                <Button disabled= {isLoading} variant={"primary"}>
+                    Create
+                </Button>
+            </DialogFooter>
+                </form>
+            </Form>
+            </DialogContent>
+            
         </Dialog>
     )
 }
